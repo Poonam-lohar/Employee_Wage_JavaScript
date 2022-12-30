8a3a7d362042613e515aea6d832b5dd769082a0a
Your Name <you@example.com>
2022-12-30 15:05:38 +0530

    Added: Refactor code for write function for daily wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,3 +1,5 @@
+//UC1
+
 const IS_ABSENT = 0
 let empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == IS_ABSENT) {
@@ -7,6 +9,9 @@ if (empCheck == IS_ABSENT) {
     console.log("Employee is Present ");
     
 }
+
+//UC2
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -27,3 +32,20 @@ switch (empCheck) {
 let empwage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empwage);
 
+//UC3
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+                default:
+                    return 0;
+    }
+}
+empHrs = 0;
+empCheck = Math.floor(Math.random()   * 10) %3;
+empHrs = getWorkingHours(empCheck);
+empwage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empwage);
